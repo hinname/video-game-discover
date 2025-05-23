b3b383e344f5c0c386e50a32100000202dac8be8
Hinkami <herivelton.pro@gmail.com>
2025-05-23 16:29:14 -0300

    testing gh action backend

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello, you've requesteds: %s\n", r.URL.Path)
 	})
 
 	log.Fatal(http.ListenAndServe(":80", nil))
